dbf6ae7509df5c48670d6e56a685db794c753cb4
lxydiy <1452206376@qq.com>
2024-07-20 22:51:21 +0800

    修复Windows下回车被自动替换的Bug

--- src/fs_block.c
+++ src/fs_block.c
@@ -27,7 +27,7 @@ FS_STATUS fs_block_fill_crc32(fs_block_description_t *block)
 FS_STATUS fs_block_open(fs_block_description_t *block, char *filename, uint32_t blocksize)
 {
     TRUE_THEN_RETURN_FALSE(block == NULL);
-    block->fp = fopen(filename, "r+");
+    block->fp = fopen(filename, "rb+");
     TRUE_THEN_RETURN_FALSE(block->fp == NULL);
     block->blocksize = blocksize;
     block->current_block = 0;
