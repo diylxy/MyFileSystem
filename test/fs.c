0cad3595180d31d601b97ef17c5aacb7a52765b9
lxydiy <1452206376@qq.com>
2024-08-08 14:44:55 +0800

    添加命令行界面

--- test/fs.c
+++ test/fs.c
@@ -3,40 +3,47 @@
 
 #define DISK_SIZE (1024 * 1024 * 4)
 #define BLOCK_SIZE 1024
-int main()
+int main(int argc, char *argv[])
 {
+    if (argc < 3)
+    {
+        printf("Usage: %s <disk> <directory>\n", argv[0]);
+        return 0;
+    }
     simplefs_disk_handle_t *fs;
     bool need_format;
-    sfs_disk_create_empty("disk", DISK_SIZE);
-    fs = sfs_diskopen("disk", &need_format);
+    // sfs_disk_create_empty("disk", DISK_SIZE);
+    // fs = sfs_diskopen("disk", &need_format);
+    fs = sfs_diskopen(argv[1], &need_format);
     if(need_format)
     {
         printf("设备需要格式化\n");
-        sfs_diskformat(fs, DISK_SIZE, BLOCK_SIZE, "SimpleFS");
+        return 0;
+        //sfs_diskformat(fs, DISK_SIZE, BLOCK_SIZE, "SimpleFS");
     }
     printf("虚拟块设备已打开\n");
     printf("可用空间：%d\n", sfs_diskfree(fs));
-    // 创建文件
-    printf("创建文件\n");
-    sfs_fcreate(fs, "/测试1.bin");
-    simplefs_file_t *fp = sfs_fopen(fs, "/测试1.bin", MODE_WRITE);
-    sfs_fwrite("Hello World!\n123", 16, fp);
-    sfs_fclose(fp);
-    printf("创建目录 /testdir\n");
-    sfs_mkdir(fs, "/testdir");
-    printf("创建目录 /testdir/123\n");
-    sfs_mkdir(fs, "/testdir/123");
-    printf("创建文件 /test2.bin\n");
-    sfs_fcreate(fs, "/test2.bin");
-    printf("创建文件 /testdir/123/test5.bin\n");
-    sfs_fcreate(fs, "/testdir/123/test5.bin");
-    // 删除目录
-    printf("删除目录 /testdir/123\n");
-    sfs_tree_rmdir(fs, "/testdir/123");
+    // // 创建文件
+    // printf("创建文件\n");
+    // sfs_fcreate(fs, "/测试1.bin");
+    // simplefs_file_t *fp = sfs_fopen(fs, "/测试1.bin", MODE_WRITE);
+    // sfs_fwrite("Hello World!\n123", 16, fp);
+    // sfs_fclose(fp);
+    // printf("创建目录 /testdir\n");
+    // sfs_mkdir(fs, "/testdir");
+    // printf("创建目录 /testdir/123\n");
+    // sfs_mkdir(fs, "/testdir/123");
+    // printf("创建文件 /test2.bin\n");
+    // sfs_fcreate(fs, "/test2.bin");
+    // printf("创建文件 /testdir/123/test5.bin\n");
+    // sfs_fcreate(fs, "/testdir/123/test5.bin");
+    // // 删除目录
+    // printf("删除目录 /testdir/123\n");
+    // sfs_tree_rmdir(fs, "/testdir/123");
     // 遍历目录
     printf("遍历目录\n");
     simplefs_dir_t *root;
-    root = sfs_dir_open(fs, "/");
+    root = sfs_dir_open(fs, argv[2]);
     simplefs_tree_read_result_t tree_walk_result;
     printf("  类型      块号      创建时间    修改时间      大小    文件名\n");
     while(sfs_tree_readdir(root, &tree_walk_result))
